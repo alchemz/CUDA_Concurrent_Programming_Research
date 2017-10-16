a3f20cbe0a2ba39386e4f33369b57cc0f4b05cc5
Xianling(Lily) Zhang <alchemxz@gmail.com>
2017-10-16 12:16:28 -0400

    compare naive increment and atomic increment operations

---
+++ c-program/practise/read-write-modify-operation.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "gputime.h"
+
+#define NUM_THREADS 1000000
+#define ARRAY_SIZE 10
+
+#define BLOCK_WIDTH 1000
+
+void print_array(int *array, int size)
+{
+
+}
+
+__global__ void increment_naive(int *g)
+{
+	//which thread is this?
+	ing i= blockIdx.x * blockDim.x + threadIdx.x;
+
+	//each thread to increment consecutive elements, wrapping at ARRAY_SIZE
+	i = i % ARRAY_SIZE;
+	g[i] = g[i] + i;
+}
+
+int main( int argc, char **argv){
+	GpuTimer timer;
+	printf("%d total threads in %d blocks writing into %d array elements\n",
+			NUM_THREADS, NUM_THREADS / BLOCK_WIDTH, ARRAY_SIZE);
+
+	//declare and allocate host memory
+	int h_array[ARRAY_SIZE];
+	const int ARRAY_SIZE = ARRAY_SIZE * sizeof(int);
+
+	//declare, allocate, and zero out GPU memory
+	int * d_array;
+	cudaMalloc((void **) &d_array, ARRAY_BYTES);
+	cudaMemset((void *) d_array, 0, ARRAY_BYTES);
+
+	//launch the kernel
+	timer.Start();
+	increment_naive<<<NUM_THREADS/BLOCK_WIDTH, BLOCK_WIDTH>>>(d_array);
+	timer.stop();
+
+	//copy back the array of sums from GPU and print
+	cudaMemcpy(h_array, d_array, ARRAY_BYTES, cudaMemcpyDeviceToHost);
+	//print_array(h_array, ARRAY_SIZE);
+	printf("Time elapsed =%g ms\n", timer.Elapsed());
+
+	//free GPU memory allocated and exit
+	cudaFree(d_array);
+	return 0;
+
+}
